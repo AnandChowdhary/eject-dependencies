63f2573d5d3172798cb6632ae6e9a24805211760
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-25 22:27:12 +0530

    :sparkles: Basic dependency copying

--- index.ts
+++ index.ts
@@ -1,4 +1,39 @@
 import { join } from "path";
-import { readJson } from "fs-extra";
+import { readJson, pathExists, ensureDir, copy } from "fs-extra";
 
-export const eject = async () => {};
+/**
+ * `console.log` messages if in development environment
+ * @param params - Messages to log
+ */
+const log = (...params: any[]) =>
+  process.env.NODE_ENV === "development" && console.log(...params);
+
+export interface EjectSettings {
+  sourceDir?: string;
+  destDir?: string;
+}
+
+/**
+ * Eject dependencies
+ */
+export const eject = async (settings: EjectSettings = {}) => {
+  const nodeModulesDir = settings.sourceDir || join(".", "node_modules");
+  const destDir = settings.destDir || join(".", "ejected");
+
+  await ensureDir(destDir);
+  if (!(await pathExists(nodeModulesDir)))
+    throw new Error("node_modules not found");
+
+  const pkg: {
+    dependencies: {
+      [index: string]: string;
+    };
+  } = await readJson(join(".", "package.json"));
+
+  for await (const x of Object.keys(pkg.dependencies)) {
+    log("Copying dependency", x);
+    await copy(join(nodeModulesDir, x), destDir);
+  }
+};
+
+eject();
