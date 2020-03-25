6a49894d3ae35063f6ae0a80331a3be7f58b730e
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-26 00:36:49 +0530

    :sparkles: Add CLI

--- cli.ts
+++ cli.ts
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import yargs from "yargs";
+import { eject, EjectSettings } from "./";
+const argv = yargs.argv as EjectSettings;
 
-console.log("ARGUMENTS", process.argv);
+eject(argv);
